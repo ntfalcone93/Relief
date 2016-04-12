23de349a15a5f4dd8390569342d9773c8a3dc3c3
Kaelin Osmun <KaelinO@gmail.com>
2016-04-12 16:35:38 -0600

    Event Model
    Event Model Complete, dictionary w/ Jake's create dictionary array Extension method.

--- Event.swift
+++ Event.swift
@@ -9,9 +9,25 @@
 import Foundation
 import UIKit
 
-enum Type {
-    case
-    
+enum EventType: String {
+    case Epidemic
+    case AnimalAndInsectInfestation
+    case Earthquakes
+    case DryMassMovement
+    case Tsunamis
+    case VolcanicEruptions
+    case Drought
+    case ExtremeTemperatues
+    case WildfiresAndUrbanFires
+    case Floods
+    case WetMassMovement
+    case TropicalStormsHurricanesTyphoonsAndCyclones
+    case StormsAndTidalWaves
+    case IndustrialAccidents
+    case TransportAccidents
+    case ComplexEmergencies
+    case FamineOrFoodInsecurity
+    case DisplacedPopulations
 }
 
 class Event {
@@ -23,47 +39,70 @@ class Event {
     private let identifierKey = "identifier"
     private let endpointKey = "endpoint"
     private let jsonValueKey = "jsonValue"
+    private let eventTypeKey = "eventTypeKey"
     
     var title: String
     var collectionPoint: String
     var memberCount: Int
     var needs: [String]
     var identifier: String?
-    var endpoint: String
-    var jsonValue: [String:AnyObject]
+    var endpoint = "events"
+    var type: EventType.RawValue
+    
+    var jsonValue: [String:AnyObject] {
 
-    init(title: String, collectionPoint: String, memberCount: Int, needs: [String], identifier: String?, endpoint: String, jsonValue: [String: AnyObject]) {
-        self.title = title
-        self.collectionPoint = collectionPoint
-        self.memberCount = memberCount
-        self.needs = needs
-        self.identifier = identifier
-        self.endpoint = endpoint
-        self.jsonValue = jsonValue
-    }
-
-
-    init?(dictionary: Dictionary<String, AnyObject>) {
-        guard let title = dictionary[titleKey] as? String,
-            let collectionPoint = dictionary[collectionPointKey] as? String,
-            let memberCount = dictionary[memberCountKey] as? Int,
-            let endpoint = dictionary[endpointKey] as? String else {
-
-                return nil
-                
-        }
-        
-        self.title = ""
-        self.collectionPoint = ""
-        self.memberCount = memberCount
-        self.endpoint = ""
-        
+        return [titleKey: title, eventTypeKey: type, collectionPointKey: collectionPoint, memberCountKey: memberCount, needsKey: needs.toDic()]
+    
     }
     
-    init(title: String, type: Type, collectionPoint: String) {
-        self.title = title
-        
-        self.collectionPoint = collectionPoint
-
+    init(title: String, collectionPoint: String, memberCount: Int, needs: [String], identifier: String?, endpoint: String, eventType: EventType) {
+    self.title = title
+    self.collectionPoint = collectionPoint
+    self.memberCount = memberCount
+    self.needs = needs
+    self.identifier = identifier
+    self.endpoint = endpoint
+    self.type = eventType.rawValue
+    }
+    
+    
+    init?(dictionary: Dictionary<String, AnyObject>) {
+    guard let title = dictionary[titleKey] as? String,
+    let collectionPoint = dictionary[collectionPointKey] as? String,
+    let memberCount = dictionary[memberCountKey] as? Int,
+    let needs = dictionary[needsKey] as? [String],
+    let endpoint = dictionary[endpointKey] as? String,
+    let eventType = dictionary[eventTypeKey] as? String else {
+    
+    return nil
+    
+    }
+    
+    self.title = title
+    self.collectionPoint = collectionPoint
+    self.memberCount = memberCount
+    self.endpoint = endpoint
+    self.needs = needs
+    self.type = eventType
+    
+    }
+    
+    init(title: String, type: EventType, collectionPoint: String) {
+    self.title = title
+    self.type = type.rawValue
+    self.collectionPoint = collectionPoint
+    self.memberCount = 1
+    self.needs = []
+    self.identifier = nil
+    }
 }
+
+extension Array {
+    func toDic() -> [String : AnyObject] {
+        var dicToReturn = [String : AnyObject]()
+        for item in self {
+            dicToReturn.updateValue(true, forKey: String(item))
+        }
+        return dicToReturn
+    }
 }
