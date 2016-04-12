d88ab9d8c861ffca8c51031f57e42d19b1843e27
Kaelin Osmun <KaelinO@gmail.com>
2016-04-12 15:00:07 -0600

    Event Model
    keys, properties, dictionaries

--- Event.swift
+++ Event.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import UIKit
+
+enum Type {
+    case
+    
+}
+
+class Event {
+    
+    private let titleKey = "title"
+    private let collectionPointKey = "collectionPoint"
+    private let memberCountKey = "memberCount"
+    private let needsKey = "memberCount"
+    private let identifierKey = "identifier"
+    private let endpointKey = "endpoint"
+    private let jsonValueKey = "jsonValue"
+    
+    var title: String
+    var collectionPoint: String
+    var memberCount: Int
+    var needs: [String]
+    var identifier: String?
+    var endpoint: String
+    var jsonValue: [String:AnyObject]
+
+    init(title: String, collectionPoint: String, memberCount: Int, needs: [String], identifier: String?, endpoint: String, jsonValue: [String: AnyObject]) {
+        self.title = title
+        self.collectionPoint = collectionPoint
+        self.memberCount = memberCount
+        self.needs = needs
+        self.identifier = identifier
+        self.endpoint = endpoint
+        self.jsonValue = jsonValue
+    }
+
+
+    init?(dictionary: Dictionary<String, AnyObject>) {
+        guard let title = dictionary[titleKey] as? String,
+            let collectionPoint = dictionary[collectionPointKey] as? String,
+            let memberCount = dictionary[memberCountKey] as? Int,
+            let endpoint = dictionary[endpointKey] as? String else {
+
+                return nil
+                
+        }
+        
+        self.title = ""
+        self.collectionPoint = ""
+        self.memberCount = memberCount
+        self.endpoint = ""
+        
+    }
+    
+    init(title: String, type: Type, collectionPoint: String) {
+        self.title = title
+        
+        self.collectionPoint = collectionPoint
+
+}
+}
