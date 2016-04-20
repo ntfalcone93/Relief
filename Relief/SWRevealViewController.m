153916bb3e2a9a62b48c0038059c2c5bd5ccae80
Kaelin Osmun <KaelinO@gmail.com>
2016-04-20 10:27:01 -0600

    Erroneous RevealViewController
    Got rid of it, plus SWRevealViewController was having issues with Obj-C file because of compiling two separate xCode versions. Added code fix in lines 764-771 in SWRevealViewController.

--- Relief/SWRevealViewController.m
+++ Relief/SWRevealViewController.m
@@ -761,14 +761,14 @@ const int FrontViewPositionNone = 0xff;
     _userInteractionStore = _contentView.userInteractionEnabled;
 }
 
-
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
 - (NSUInteger)supportedInterfaceOrientations
+#else
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+#endif
 {
-    // we could have simply not implemented this, but we choose to call super to make explicit that we
-    // want the default behavior.
-    return [super supportedInterfaceOrientations];
+    return UIInterfaceOrientationMaskPortrait;
 }
-
 
 #pragma mark - Public methods and property accessors
 
