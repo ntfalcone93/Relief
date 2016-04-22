979cf82405b4212d63e727b6a27a07c7a73b6036
Jake Hardy <jhardy3@live.com>
2016-04-22 11:44:01 -0600

    finished cleaning up chat

---
+++ Relief/ChatTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ChatTableViewCell.swift
+//  Relief
+//
+//  Created by Jake Hardy on 4/22/16.
+//  Copyright © 2016 Relief Group. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var usernameTextLabel: UILabel!
+    @IBOutlet weak var userMessageTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
