76999c80f24b5b0d19c83517a9c56722e8fb78b6
Jake Hardy <jhardy3@live.com>
2016-04-25 09:37:54 -0600

    Changed message layout

--- Relief/FeedViewController.swift
+++ Relief/FeedViewController.swift
@@ -57,7 +57,7 @@ class FeedViewController: UIViewController, FirebaseChatManager, UITextFieldDele
     @IBAction func sendButtonTapped(sender: UIButton) {
         let firstName = UserController.sharedInstance.currentUser.firstName
         let lastName = UserController.sharedInstance.currentUser.lastName ?? ""
-        chatManager?.messagePosted(messageTextField, username: "\(firstName)\(lastName)")
+        chatManager?.messagePosted(messageTextField, username: "- \(firstName.capitalizedString) \(lastName.capitalizedString)")
         textFieldShouldReturn(messageTextField)
     }
     
@@ -112,8 +112,8 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCellWithIdentifier("chatCell", forIndexPath: indexPath) as? ChatTableViewCell else { return tableView.dequeueReusableCellWithIdentifier("chatCell", forIndexPath: indexPath)}
         let message = chatManager?.messages[indexPath.row]
-        cell.usernameTextLabel?.text = message?.username
-        cell.userMessageTextLabel?.text = message?.messageBodyText
+        cell.usernameTextLabel?.text = message?.messageBodyText
+        cell.userMessageTextLabel?.text = message?.username
         return cell
     }
     
